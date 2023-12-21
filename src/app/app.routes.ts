875a43f3e86fd1c5f510152b706517815eb6f6f2
andresisfx <124216395+andresisfx@users.noreply.github.com>
2023-12-21 17:16:59 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { CertificadosComponent } from './certificados/certificados.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:InicioComponent
+      },
+      {
+        path:"servicios",
+        component:ServiciosComponent
+      },
+      {
+        path:"empresa",
+        component:EmpresaComponent
+    
+      },
+      {
+        path:"certificados",
+        component:CertificadosComponent
+      }
+];
