f45a540c354285536e730495dc08a72712a57777
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-01-11 16:01:59 -0500

    certificados firebase app created

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"yadiramayac","appId":"1:1051223021501:web:fb5efb32218b051936e4d4","storageBucket":"yadiramayac.appspot.com","locationId":"us-central","apiKey":"AIzaSyDMAa0zIJp3SckM8nWsJHRdswZ1fiEvzrU","authDomain":"yadiramayac.firebaseapp.com","messagingSenderId":"1051223021501"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
