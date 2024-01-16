d3005403cb4a2f6af8b229637f311ec7a63a7ba1
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-01-15 20:21:36 -0500

    upload certificados ready

--- src/app/certificados/certificados.component.ts
+++ src/app/certificados/certificados.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Storage,ref,uploadBytes} from '@angular/fire/storage';
 @Component({
   selector: 'app-certificados',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
 export class CertificadosComponent {
+  constructor(private storage:Storage){
 
+  }
+  uploadFile($event:any){
+    const file = $event.target.files[0]
+    const pdfRef= ref(this.storage,`certificados/${file.name}`)
+    
+    uploadBytes(pdfRef,file).then(x=>{
+      console.log(x)
+    }).catch(error=>console.log(error))
+  }
 }
