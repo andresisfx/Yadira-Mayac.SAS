1a9aca87380c5d5b9f501937cdb904fbc203192b
Jairorb-06 <jairorosero2@udenar.edu.co>
2024-01-17 22:58:28 -0500

    changes functionality when uploading multiple pdfs in folders

--- src/app/certificados/certificados.component.ts
+++ src/app/certificados/certificados.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Storage,ref,uploadBytes,listAll,getDownloadURL} from '@angular/fire/storage';
 @Component({
@@ -18,22 +18,109 @@ export class CertificadosComponent implements OnInit {
   }
   uploadFile($event:any){
     const file = $event.target.files[0]
+    console.log("filllle", file)
     const pdfRef= ref(this.storage,`certificados/${file.name}`)
 
     uploadBytes(pdfRef,file).then(x=>{
       console.log(x)
     }).catch(error=>console.log(error))
   }
+
   getPdf(){
     const certificadosRef= ref(this.storage,`certificados`);
     listAll(certificadosRef).then(async certs=>{
+      this.pdfs=[];
       for(let cert of certs.items ){
-        this.pdfs=[];
         const url = await getDownloadURL(cert);
         this.pdfs.push(url);
         console.log(this.pdfs)
         
       }
     }).catch(error=>console.log(error)) 
+  }
+}
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage';
+
+@Component({
+  selector: 'app-certificados',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './certificados.component.html',
+  styleUrl: './certificados.component.css'
+})
+export class CertificadosComponent implements OnInit {
+  selectedFiles: any;
+  loading= false;
+  constructor(private storage: Storage) {}
+
+  pdfs: string[] = [];
+  
+  ngOnInit(): void {
+    this.getPdf();
+  }
+
+   uploadFiles($event: any) {
+    const files: FileList = $event.target.files;
+    if (files.length === 0) {
+      return; 
+    }
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      this.loading = true
+      
+      this.uploadFile(file);
+    }
+  } 
+  
+  
+  uploadFile = (file: File) => {
+    console.log("loading ...", this.loading)
+    const folderName = file.name.replace(/\.[^/.]+$/, ""); 
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().replace(/[-T:.Z]/g, "");
+    const fileNameWithDate = `${formattedDate}_${file.name}`;
+    const pdfRef = ref(this.storage, `${folderName}/${fileNameWithDate}`);
+
+    //veo si carpeta existe
+    const folderRef = ref(this.storage, folderName);
+    listAll(folderRef)
+      .then(() => {
+        this.uploadFileInsideFolder(pdfRef, file);
+      })
+      .catch(() => {
+        // nuva carpeta si no hay 
+        const createFolderRef = ref(this.storage, folderName);
+        uploadBytes(createFolderRef, new Blob()).then(() => {
+          this.uploadFileInsideFolder(pdfRef, file);
+        }).catch(error =>{
+          this.loading=false;
+           console.log(error)
+        });
+      });
+  }
+
+  uploadFileInsideFolder(pdfRef: any, file: File) {
+    uploadBytes(pdfRef, file).then(x => {
+      this.loading=false;
+      console.log(x);
+      console.log("loading ...", this.loading)
+    }).catch(error => {
+      this.loading=false;
+      console.log(error)});
+  }
+
+  getPdf() {
+    const certificadosRef = ref(this.storage, `certificados`);
+    listAll(certificadosRef).then(async certs => {
+      for (let cert of certs.items) {
+        const url = await getDownloadURL(cert);
+        this.pdfs.push(url);
+        console.log(this.pdfs);
+      }
+    }).catch(error => console.log(error));
   }
 }
