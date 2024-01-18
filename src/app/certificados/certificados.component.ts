279c3ba66c6e83aa5f4b2d03bbd93be98d464521
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-01-17 21:20:19 -0500

    upload files with id

--- src/app/certificados/certificados.component.ts
+++ src/app/certificados/certificados.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Storage,ref,uploadBytes,listAll,getDownloadURL} from '@angular/fire/storage';
+import { listAll,getDownloadURL,ref, uploadBytes, getStorage, StorageReference, Storage} from '@angular/fire/storage';
+
 @Component({
   selector: 'app-certificados',
   standalone: true,
@@ -9,20 +10,46 @@ import { Storage,ref,uploadBytes,listAll,getDownloadURL} from '@angular/fire/sto
   styleUrl: './certificados.component.css'
 })
 export class CertificadosComponent implements OnInit {
-  constructor(private storage:Storage){
-
+  private fileCounter = 1; // Contador de archivos
+  constructor(private storage:Storage ){
+    
   }
   pdfs:string[]=[]
   ngOnInit(): void {
     this.getPdf()
   }
-  uploadFile($event:any){
-    const file = $event.target.files[0]
-    const pdfRef= ref(this.storage,`certificados/${file.name}`)
+  async uploadFile($event: any) {
+    const file = $event.target.files[0];
+      const folderPath = `${file.name}/`;
+      const folderRef = ref(this.storage,folderPath);
+      const folderExists = await listAll(folderRef)
+       // Inicializa el servicio de almacenamiento
+      console.log(folderExists)
+      const fileId= this.generateUniqueId()
+      const fileName = `${fileId}_${Date.now()}_${file.name}`;
+       console.log(fileName)
+      // Incrementa el contador para el siguiente archivo
+      
+      // Construye la referencia al archivo en el Storage de Firebase
+      const pdfRef = ref(this.storage, `${file.name}/${fileName}`);
+     try {
+        // Sube el archivo al Storage
+        const uploadResult = await uploadBytes(pdfRef, file);
+        // Almacena el nuevo valor del contador en la base de datos
+        // Obtiene la URL de descarga del archivo
+        const downloadURL = await getDownloadURL(pdfRef);
 
-    uploadBytes(pdfRef,file).then(x=>{
-      console.log(x)
-    }).catch(error=>console.log(error))
+        console.log('Archivo subido con éxito:', uploadResult);
+        console.log('URL de descarga:', downloadURL);
+      } catch (error) {
+        console.error('Error al subir el archivo:', error);
+      }
+    
+  }
+  generateUniqueId(): string {
+    const timestamp = new Date().getTime();
+    const randomString = Math.random().toString(36).substring(2, 8);
+    return `${timestamp}_${randomString}`;
   }
   getPdf(){
     const certificadosRef= ref(this.storage,`certificados`);
