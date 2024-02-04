94a1b0037e44c9a8b9e83c619465f91373eaee94
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-02-03 20:41:37 -0500

    contactanos and trabaja con nosotros components added

--- src/app/trabaja-con-nosotros/trabaja-con-nosotros.component.ts
+++ src/app/trabaja-con-nosotros/trabaja-con-nosotros.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-trabaja-con-nosotros',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './trabaja-con-nosotros.component.html',
   styleUrl: './trabaja-con-nosotros.component.css'
 })
